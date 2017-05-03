25ddfbe1d5f7b850dcff8571be3b0fa9de35fa79
Rsilnav <rafael.silnav@gmail.com>
2017-05-03 15:53:00 +0200

    Deleted some comments

--- solutions/8/main.py
+++ solutions/8/main.py
@@ -2,12 +2,9 @@ def transform(caracteres):
     try:
         return '{0:1x}'.format(int(caracteres))
     except:
-        #for c in line:
-        #    print repr(c), ord(c)
         return "N/A"
 
 import io
 file = io.open("submitInput.txt",'r',encoding='utf-16-le')
 for i, line in enumerate(file.readlines()[1:]):
-    #print line.strip()
     print "Case #" + str(i+1) + ": " + transform(line.strip())
